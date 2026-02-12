6f89efb4e7ff89c1c7fbfca5166b878863c36f46
mepul <sepulvedamorgan2@gmail.com>
2026-02-12 13:49:55 -0600

    Feature: Create parallel lists to store data

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@ if (!File.Exists(file))
 }
 else
 {
+    // create parallel lists of character details
+    // lists are used since we do not know number of lines of data
+    List<UInt64> Ids = [];
+    List<string> Names = [];
+    List<string> Descriptions = [];
     string? choice;
     do
     {
