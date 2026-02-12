43c17c964445b3d47cbe2e52d92e91759b2be2f6
mepul <sepulvedamorgan2@gmail.com>
2026-02-12 13:47:38 -0600

    Feature: Create User Menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿﻿using NLog;
-string path = Directory.GetCurrentDirectory() + "//nlog.xml";
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
@@ -14,7 +14,27 @@ if (!File.Exists(file))
 }
 else
 {
-    // TODO: create user menu
+    string? choice;
+    do
+    {
+        // display choices to user
+        Console.WriteLine("1) Add Character");
+        Console.WriteLine("2) Display All Characters");
+        Console.WriteLine("Enter to quit");
+
+        // input selection
+        choice = Console.ReadLine();
+        logger.Info("User choice: {Choice}", choice);
+
+        if (choice == "1")
+        {
+            // Add Character
+        }
+        else if (choice == "2")
+        {
+            // Display All Characters
+        }
+    } while (choice == "1" || choice == "2");
 }
 
 logger.Info("Program ended");
