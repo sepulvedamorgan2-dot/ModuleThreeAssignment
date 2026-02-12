754e23878f975c8679a7dd26c374b938745a4eb8
mepul <sepulvedamorgan2@gmail.com>
2026-02-12 14:08:16 -0600

    Feature: Ensure each name is unique

--- Program.cs
+++ Program.cs
@@ -63,14 +63,20 @@ else
             // Add Character
             Console.WriteLine("Enter new character name: ");
             string? Name = Console.ReadLine();
-            if (!string.IsNullOrEmpty(Name))
-            {
-                // generate id - use max value in Ids + 1
-                UInt64 Id = Ids.Max() + 1;
-                Console.WriteLine($"{Id}, {Name}");
-            }
-            else
-            {
+            if (!string.IsNullOrEmpty(Name)){
+                // check for duplicate name
+                List<string> LowerCaseNames = Names.ConvertAll(n => n.ToLower());
+                if (LowerCaseNames.Contains(Name.ToLower()))
+                {
+                    logger.Info($"Duplicate name {Name}");
+                }
+                else
+                {
+                    // generate id - use max value in Ids + 1
+                    UInt64 Id = Ids.Max() + 1;
+                    Console.WriteLine($"{Id}, {Name}");
+                }
+            } else {
                 logger.Error("You must enter a name");
             }
         }
